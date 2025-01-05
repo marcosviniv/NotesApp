173163297d9a7aa1409897a6577c6d7e70689195
Code Marcos <marcosv.code@gmail.com>
2025-01-05 20:47:16 -0300

    Created NotesView and NoteViewModel.

---
+++ NotesApp/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  NotesApp
+//
+//  Created by Marcos Vinícius Vieira on 05/01/25.
+//
+
+import Foundation
---
+++ NotesApp/Notes/ViewModel/NoteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteViewModel.swift
+//  NotesApp
+//
+//  Created by Marcos Vinícius Vieira on 05/01/25.
+//
+
+import Foundation
