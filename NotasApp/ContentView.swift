1bff7c0d56e0ec0f011b75e07c3f6ac730d88f69
Code Marcos <marcosv.code@gmail.com>
2025-01-05 00:44:13 -0300

    Initial Commit

---
+++ NotasApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotasApp
+//
+//  Created by Marcos Vinícius Vieira on 05/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotasApp/NotasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasAppApp.swift
+//  NotasApp
+//
+//  Created by Marcos Vinícius Vieira on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NotasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
