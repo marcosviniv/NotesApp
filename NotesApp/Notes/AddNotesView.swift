6b62c5fe458c26ab146eebcdfc36873e9d70fab1
Code Marcos <marcosv.code@gmail.com>
2025-01-08 00:06:12 -0300

    Add NotesView.

---
+++ NotesApp/Notes/AddNotesView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNotesView.swift
+//  NotesApp
+//
+//  Created by Marcos Vinícius Vieira on 07/01/25.
+//
+
+import SwiftUI
+
+struct AddNotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNotesView()
+}
